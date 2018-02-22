89769a7ddb829baf54192fe62e90a5f7deb25e16
Shannon Bride <sbride@mines.edu>
2018-02-22 12:15:41 -0700

    Changed text

--- src/Cat.java
+++ src/Cat.java
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow!", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Thank goodness we are finished! xoxo", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
